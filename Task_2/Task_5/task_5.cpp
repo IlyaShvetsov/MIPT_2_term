5cf674f8dca26b388c184c11d4cb412ce401017f
Ilya <shvetsov.ilya@yandex.ru>
2019-12-08 21:48:34 +0300

    2_term

--- Task_2/Task_5/task_5.cpp
+++ Task_2/Task_5/task_5.cpp
@@ -6,92 +6,93 @@ using namespace std;
 
 class CAdjacencyMatrix {
 public:
-	explicit CAdjacencyMatrix(int _verticesCount);
-	~CAdjacencyMatrix() {}
+    explicit CAdjacencyMatrix(int _verticesCount);
+    ~CAdjacencyMatrix() = default;
 
-	virtual void AddEdge(int from, int to, int len);
-	virtual int VerticesCount() const { return verticesCount; }
-	virtual void GetNextVertices(int vertex, vector<int>& vertices) const;
-	virtual void GetPrevVertices(int vertex, vector<int>& vertices) const;
+    void AddEdge(int from, int to, int len);
+    int VerticesCount() const { return verticesCount; }
+    void GetNextVertices(int vertex, vector<int>& vertices) const;
+    void GetPrevVertices(int vertex, vector<int>& vertices) const;
 
 private:
-	int verticesCount;
-	vector<vector<int> > edges;
+    int verticesCount;
+    vector<vector<int> > edges;
 };
 
 CAdjacencyMatrix::CAdjacencyMatrix(int _verticesCount) :
-	verticesCount(_verticesCount),
-	edges(verticesCount)
-{
-	for (int i = 0; i < verticesCount; ++i)
-		edges[i].resize(verticesCount);
+        verticesCount(_verticesCount),
+        edges(verticesCount) {
+    for (int i = 0; i < verticesCount; ++i)
+        edges[i].resize(verticesCount);
 }
 
-void CAdjacencyMatrix::AddEdge(int from, int to, int len)
-{
-	edges[from][to] = len;
+void CAdjacencyMatrix::AddEdge(int from, int to, int len) {
+    edges[from][to] = len;
 }
 
-void CAdjacencyMatrix::GetNextVertices(int vertex, vector<int>& vertices) const
-{
-	for (int i = 0; i < verticesCount; ++i)
-		vertices.push_back(edges[vertex][i]);
+void CAdjacencyMatrix::GetNextVertices(int vertex, vector<int>& vertices) const {
+    for (int i = 0; i < verticesCount; ++i) {
+        vertices.push_back(edges[vertex][i]);
+    }
 }
 
-void CAdjacencyMatrix::GetPrevVertices(int vertex, vector<int>& vertices) const
-{
-	for (int i = 0; i < verticesCount; ++i)
-		vertices.push_back(edges[i][vertex]);
-}
-
-
-
-int min(int a, int b)
-{
-	if (a < b)
-		return a;
-	else
-		return b;
-}
-
-void Floyd(const CAdjacencyMatrix& Graph, vector<vector<int> >& distances)
-{
-	for (int i = 0; i < Graph.VerticesCount(); ++i)
-		Graph.GetNextVertices(i, distances[i]);
-	for (int k = 0; k < Graph.VerticesCount(); ++k)
-		for (int i = 0; i < Graph.VerticesCount(); ++i)
-			for (int j = 0; j < Graph.VerticesCount(); ++j)
-				distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+void CAdjacencyMatrix::GetPrevVertices(int vertex, vector<int>& vertices) const {
+    for (int i = 0; i < verticesCount; ++i) {
+        vertices.push_back(edges[i][vertex]);
+    }
 }
 
 
 
-int main()
-{
-	int n, edge;
-	ifstream inp("floyd.in");
-	ofstream out("floyd.out");
-	inp >> n;
-	CAdjacencyMatrix graph(n);
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-		{
-			inp >> edge;
-			graph.AddEdge(i, j, edge);
-		}
+int min(int a, int b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
 
-	vector<vector<int> > distances;
-	distances.resize(n);
-	Floyd(graph, distances);
+void Floyd(const CAdjacencyMatrix& Graph, vector<vector<int> >& distances) {
+    for (int i = 0; i < Graph.VerticesCount(); ++i) {
+        Graph.GetNextVertices(i, distances[i]);
+    }
 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-			out << distances[i][j] << ' ';
-		out << endl;
-	}
+    for (int k = 0; k < Graph.VerticesCount(); ++k) {
+        for (int i = 0; i < Graph.VerticesCount(); ++i) {
+            for (int j = 0; j < Graph.VerticesCount(); ++j) {
+                distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+            }
+        }
+    }
+}
 
-	inp.close();
-	out.close();
-	return 0;
+
+
+int main() {
+    int n, edge;
+    ifstream inp("floyd.in");
+    ofstream out("floyd.out");
+    inp >> n;
+    CAdjacencyMatrix graph(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            inp >> edge;
+            graph.AddEdge(i, j, edge);
+        }
+    }
+
+    vector<vector<int> > distances;
+    distances.resize(n);
+    Floyd(graph, distances);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            out << distances[i][j] << ' ';
+        }
+        out << endl;
+    }
+
+    inp.close();
+    out.close();
+    return 0;
 }
